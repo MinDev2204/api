7d0077ba6c1ce4550f2e652f659745a963f4c2df
Muhammadamin G'anijonov <ganijonovmuhammadamin4@gmail.com>
2025-05-02 15:02:32 +0500

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -8,12 +8,13 @@ class User(models.Model):
         return self.userName
 
 class Product(models.Model):
-    user = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    userName = models.CharField(max_length=255)
     image = models.ImageField(upload_to='products/')
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=100)
+    status = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
@@ -24,3 +25,4 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
